1ee4c285ca0d1abe0199234bef5e5f93b7a7e2a0
janzizu <dkadnsk@naver.com>
2022-10-19 08:48:46 +0900

    알람설정완료

--- alarmCodelab/app/src/main/java/com/jisoo/alarmcodelab/util/NotificationUtils.kt
+++ alarmCodelab/app/src/main/java/com/jisoo/alarmcodelab/util/NotificationUtils.kt
@@ -5,6 +5,8 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.jisoo.alarmcodelab.MainActivity
 import com.jisoo.alarmcodelab.R
@@ -13,9 +15,25 @@ private val NOTIFICATION_ID = 0
 private val REQUEST_CODE = 0
 private val FLAGS = 0
 
+/**
+ * NotificationManager를 호출하는 확장 기능 추가...
+ * send & cancel
+ * */
+
+@RequiresApi(Build.VERSION_CODES.S)
 fun NotificationManager.sendNotification(msg: String, context: Context) {
 
+    /**
+     * 알람을 눌렀을 때 메인액티비티로 돌아가기를 원하므로...
+     * pendingIntent를 이용하여 특정상황에서 처리되도록 한다.
+     * */
     val contentIntent = Intent(context, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        context,
+        REQUEST_CODE,
+        contentIntent,
+        PendingIntent.FLAG_MUTABLE
+    )
 
     val builder = NotificationCompat.Builder(
         context,
@@ -24,5 +42,20 @@ fun NotificationManager.sendNotification(msg: String, context: Context) {
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(context.getString(R.string.notification_title))
         .setContentText(msg)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
         notify(NOTIFICATION_ID, builder.build())
+
+    val eggImage = BitmapFactory.decodeResource(
+        context.resources,
+        R.drawable.cooked_egg
+    )
+}
+
+/**
+ * 새 타이머를 시작하는 경우
+ * 기존 타이머가 있으면 기존 타이머를 지워준다.
+ * */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
 }
